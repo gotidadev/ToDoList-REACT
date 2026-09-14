29319f9ee289e252c5fae723c2967282df84ffe5
gotidadev <rafaela.olivekls@gmail.com>
2026-09-14 20:50:22 -0300

    fix: correct component imports

--- src/pages/home/App.jsx
+++ src/pages/home/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import ItemList from "../../components/ItemList";
 import "./App.css";
-import FormList from "../../components/formList";
+import FormList from "../../components/FormList";
 
 function App() {
   const [list, SetList] = useState( JSON.parse(localStorage.getItem("Names")) || []);
